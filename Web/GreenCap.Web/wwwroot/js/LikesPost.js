30388f16e2d283cd6d8ae1cb477436c43b4a7317
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-12-01 20:03:01 +0200

    ajax+jquery for categories of posts, small update in news

---
+++ Web/GreenCap.Web/wwwroot/js/LikesPost.js
@@ -0,0 +1,24 @@
+﻿$("button[data-vote]").each(function (el) {
+    $(this).click(function () {
+
+        var IsPositive = Boolean(parseInt($(this).attr("data-vote")));
+        var postId = parseInt(this.parentElement.getAttribute("id"));
+
+        var antiForgeryToken = $('#antiForgeryForm input[name=__RequestVerificationToken]').val();
+        var data = { postId: postId, IsPositive: IsPositive };
+
+        $.ajax({
+            type: "POST",
+            url: "/api/Likes",
+            data: JSON.stringify(data),
+            headers: {
+                'X-CSRF-TOKEN': antiForgeryToken
+            },
+            success: function (data) {
+                $('#disslikesValue_' + postId).html(data.dissLikes);
+                $('#likesValue_' + postId).html(data.likes);
+            },
+            contentType: 'application/json',
+        });
+    })
+});
