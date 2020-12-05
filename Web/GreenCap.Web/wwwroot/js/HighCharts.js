8390bef44fe2f818bb99f38d2970269951fd1ddf
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-12-05 20:07:40 +0200

    Revert "admin view, change views"
    This reverts commit 063f5cd59c0ae176f726447482f851055e94105f.

---
+++ Web/GreenCap.Web/wwwroot/js/HighCharts.js
@@ -0,0 +1,42 @@
+﻿
+
+Highcharts.chart('container', {
+    chart: {
+        type: 'column',
+        options3d: {
+            enabled: true,
+            alpha: 10,
+            beta: 25,
+            depth: 70
+        }
+    },
+    title: {
+        text: 'Montly users activity'
+    },
+    subtitle: {
+        text: 'Become even green by participating in the site!'
+    },
+    plotOptions: {
+        column: {
+            depth: 25
+        }
+    },
+    xAxis: {
+        categories: Highcharts.getOptions().lang.shortMonths,
+        labels: {
+            skew3d: true,
+            style: {
+                fontSize: '16px'
+            }
+        }
+    },
+    yAxis: {
+        title: {
+            text: null
+        }
+    },
+    series: [{
+        name: 'Activity',
+        data: [2, 3, null, 4, 0, 5, 1, 4, 6, 3]
+    }]
+});
