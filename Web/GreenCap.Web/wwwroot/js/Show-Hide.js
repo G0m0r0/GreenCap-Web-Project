d2706bb7d2a91c506eb23a68a50a0dba6c01d2bd
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-12-05 20:51:57 +0200

    better administrator design

---
+++ Web/GreenCap.Web/wwwroot/js/Show-Hide.js
@@ -0,0 +1,12 @@
+﻿function showhide(id) {
+    var div = document.getElementById("div_" + id);
+    var button = document.getElementById("button_" + id);
+
+    if (div.style.display === "block") {
+        div.style.display = "none";
+        button.innerHTML = "Show";
+    } else {
+        div.style.display = "block";
+        button.innerHTML = "Hide"
+    }
+} 
