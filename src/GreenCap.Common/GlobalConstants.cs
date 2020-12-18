dc0b91c4e28fcbaf8d0ad159413c5e5a2cdd7438
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-12-18 21:22:07 +0200

    added src folder

---
+++ src/GreenCap.Common/GlobalConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace GreenCap.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "GreenCap";
+
+        public const string AdministratorRoleName = "Administrator";
+
+        public const string AdministatorName = "Administrator";
+
+        public const string AdministratorEmail = "newpdb123@gmail.com";
+
+        public const string AdministratorPhoneNumber = "+359887967927";
+
+        // seeded password- changed later
+        public const string AdministratorPassword = "@Aa123.";
+
+        public const string DateTimeFormat = "dd/MMM/yyyy";
+    }
+}
